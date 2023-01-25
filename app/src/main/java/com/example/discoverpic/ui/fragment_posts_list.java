107c56a19e50b8ffe59f5a91404a4fbc4465f369
Danielle <211907670@idf.il>
2023-01-25 20:51:57 +0200

    post fragment

---
+++ app/src/main/java/com/example/discoverpic/ui/fragment_posts_list.java
@@ -0,0 +1,40 @@
+package com.example.discoverpic.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.discoverpic.databinding.FragmentPostsListBinding;
+import com.example.discoverpic.model.Post;
+
+import java.util.List;
+
+public class fragment_posts_list extends Fragment {
+    FragmentPostsListBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentPostsListBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        binding.recyclerView.setHasFixedSize(true);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+//        PostRecyclerAdapter adapter = new PostRecyclerAdapter(getLayoutInflater(),viewModel.getData().getValue());
+//        binding.recyclerView.setAdapter(adapter);
+
+        binding.progressBar.setVisibility(View.GONE);
+
+//        viewModel.getData().observe(getViewLifecycleOwner(),list->{
+//            adapter.setData(list);
+//        });
+
+        return view;
+    }
+}
